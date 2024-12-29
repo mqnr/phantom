13f1f4b0d8a2deb0802f44efb584db8af462e248
mzamorano <martin@mzamorano.com>
2024-12-29 16:43:01 -0700

    rename main class

--- src/main/java/com/mzamorano/phantom/Phantom.java
+++ src/main/java/com/mzamorano/phantom/Phantom.java
@@ -3,7 +3,7 @@ package com.mzamorano.phantom;
 import com.mzamorano.phantom.str.StringMapReader;
 import com.mzamorano.phantom.str.StringReader;
 
-public class Main {
+public class Phantom {
     public static void main(String[] args) {
         StringReader s = new StringMapReader("strings/all");
         System.out.println(s.read("helloworld"));
